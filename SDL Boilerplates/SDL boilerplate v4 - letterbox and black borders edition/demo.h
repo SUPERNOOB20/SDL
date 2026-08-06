aee7165dbfbc3e812e533ce69c24e8072d7b31ac
SUPERNOOB20 <andycia802@gmail.com>
2026-08-06 18:16:58 -0300

    Updated my boilerplate with black borders for letterbox :D

---
+++ SDL Boilerplates/SDL boilerplate v4 - letterbox and black borders edition/demo.h
@@ -0,0 +1,28 @@
+#ifndef DEMO_HEADER_FILE
+#define DEMO_HEADER_FILE
+
+
+#include <SDL3/SDL.h>
+#include <string>
+#include <cassert>
+
+#define WINDOW_WIDTH     (1280)
+#define WINDOW_HEIGHT     (720)
+
+#define WINDOW_WIDTH_F   (1280.0f)
+#define WINDOW_HEIGHT_F   (720.0f)
+
+
+
+
+SDL_FRect Background{
+    .x = 0.0f,
+    .y = 0.0f,
+    .w = WINDOW_WIDTH_F,
+    .h = WINDOW_HEIGHT_F
+};
+
+
+
+
+#endif
